bb08074da1490bae1e80a12e0926f0301d873f3e
Aleksandrs Maklakovs <aleksandrs.maklakovs@gmail.com>
2024-07-28 20:31:50 +0300

    Fix

--- backend/src/app.py
+++ backend/src/app.py
@@ -1,9 +1,9 @@
 import os
 from fastapi import FastAPI, HTTPException
 from contextlib import asynccontextmanager
-from data_models import ProjectBasic, Project, LocationBasic, Location, TagBasic, Tag, TaskBasic, Task
+from .data_models import ProjectBasic, Project, LocationBasic, Location, TagBasic, Tag, TaskBasic, Task
 from typing import List
-from mongodb_client import MongoDbClient
+from .mongodb_client import MongoDbClient
 from fastapi.encoders import jsonable_encoder
 from dotenv import load_dotenv
 from bson import ObjectId
