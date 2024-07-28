c8ee9d5f03afb9ebf8e827f101dec7ae7bcfb208
Aleksandrs Maklakovs <aleksandrs.maklakovs@gmail.com>
2024-07-28 20:33:48 +0300

    Fix

--- backend/tests/test_api.py
+++ backend/tests/test_api.py
@@ -1,9 +1,7 @@
 import pytest
 from httpx import AsyncClient, ASGITransport
-import os
-import sys
 from ..src.app import app
-from common_utils import compare_dict_without_id, clear_db, test_project_1, test_project_2, test_project_3,\
+from .common_utils import compare_dict_without_id, clear_db, test_project_1, test_project_2, test_project_3,\
                             test_location_1, test_location_2, test_location_3, test_task_1, test_task_2, test_task_3,\
                             test_tag_1, test_tag_2, test_tag_3, NOT_VALID_ID
 
