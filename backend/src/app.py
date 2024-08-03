87ae667d3b2e03cd9e45c2c1801f6fefb59cde96
Aleksandrs Maklakovs <aleksandrs.maklakovs@gmail.com>
2024-08-03 21:39:10 +0300

    [TT-3] Add Dockerfile and fix issue with pathes

--- backend/src/app.py
+++ backend/src/app.py
@@ -1,13 +1,6 @@
-import os
-from fastapi import FastAPI, HTTPException
+from fastapi import FastAPI
 from contextlib import asynccontextmanager
-from .data_models import ProjectBasic, Project, LocationBasic, Location, TagBasic, Tag, TaskBasic, Task
-from typing import List
-from .mongodb_client import MongoDbClient
-from fastapi.encoders import jsonable_encoder
-from dotenv import load_dotenv
-from bson import ObjectId
-from .routers import api_v1
+from routers import api_v1
 
 @asynccontextmanager
 async def lifespan(app: FastAPI):
