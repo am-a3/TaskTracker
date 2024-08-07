4a8784ef053623fe22afd553864485a527f73e44
Aleksandrs <aleksandrs.maklakovs@sonarworks.com>
2024-08-07 23:26:17 +0300

    [TT-25] Create blank iOS project

---
+++ ios/TaskTracker/TaskTracker/TaskTrackerApp.swift
@@ -0,0 +1,17 @@
+//
+//  TaskTrackerApp.swift
+//  TaskTracker
+//
+//  Created by Aleksandrs Maklakovs on 07/08/2024.
+//
+
+import SwiftUI
+
+@main
+struct TaskTrackerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
